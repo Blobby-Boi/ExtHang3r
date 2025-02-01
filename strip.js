603ab27a506d341a3c441accd70ab25ebdf6d44e
minidogg <117037081+minidogg@users.noreply.github.com>
2025-02-01 15:50:20 -0600

    Started adding replacers to make the page more minimal

--- strip.js
+++ strip.js
@@ -7,5 +7,15 @@ const path = require('path')
 // Get the base index.html content's so we can strip it down.
 let html = fs.readFileSync(path.resolve("./index.html")).toString()
 
+// All that good ol' regex matching magic using some home grown regex.
+html = html
+    // .replace(/<title>[\s\S]*?<\/style>/, "") // Remove all the stuff in the range from the title opening tag to the style closing tag. 
+    .replace(/<h.>.*?<\/h.>/g, "") // Remove headers
+    .replace(/<img.*?>/g, "") // Remove img elements
+    .replace(/<p>[\s\S]*?<\/p>/g, "") // Remove paragraph elements
+    .replace(/<label[\s\S]*?>[\s\S]*?<\/label>/g, "") // Remove label elements
+    .replace(">Hang Extension!</button>", ">Start!</button>") // Changes wording of kill extension button to be less obvious to someone just glancing at a user's screen.
+    .replace(">Kill Extension!</button>", ">Finish!</button>") // Changes wording of kill extension button to be less obvious to someone just glancing at a user's screen.
+
 // Write the now stripped down HTML to a new file.  
 fs.writeFileSync(path.resolve("./stripped.html"), html)
